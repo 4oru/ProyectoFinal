55d7ffef44b63e90330d5c52408cc7743b53a90c
Aorü <41136109+4oru@users.noreply.github.com>
2020-04-06 02:11:24 -0400

    Add files via upload

---
+++ api/Persisten/InventarioDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Persistence
+{   
+    public class InventarioDbContext : DbContext
+    {
+        public DbSet<Inventario> Inventario { get; set; }
+
+        public InventarioDbContext(DbContextOptions options)
+        : base(options)
+        { }
+    }
+}
+
