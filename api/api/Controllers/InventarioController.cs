efa985699009e584938ff44908ad6328718d5082
Aorü <alan_spartan_65@hotmail.com>
2020-04-06 02:39:56 -0400

    :+1:
    :P

---
+++ api/api/Controllers/InventarioController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+using Service;
+
+namespace api.Controllers
+{
+    [Route("/")]
+    public class InventarioController : Controller
+    {
+        private readonly IInventarioService _inventarioService;
+
+        public InventarioController(IInventarioService inventarioService)
+        {
+            _inventarioService = inventarioService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(
+                _inventarioService.GetAll()
+            );
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(
+                _inventarioService.Get(id)
+            );
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Inventario model)
+        {
+            return Ok(
+                _inventarioService.Add(model)
+            );
+        }
+
+        // PUT api/values/5
+        [HttpPost("{id}")]
+        public IActionResult Put([FromBody] Inventario model)
+        {
+            return Ok(
+                _inventarioService.Update(model)
+            );
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok(
+                _inventarioService.Delete(id)
+            );
+        }
+    }
+}
---
+++ api/api/Service/InventarioServices.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Service
+{
+    public interface IInventarioService
+    {
+        IEnumerable<Inventario> GetAll();
+        bool Add(Inventario model);
+        bool Delete(int id);
+        bool Update(Inventario model);
+        Inventario Get(int id);
+    }
+
+    public class InventarioService : IInventarioService
+    {
+        private readonly InventarioDbContext _inventarioDbContext;
+
+        public InventarioService(
+            InventarioDbContext inventarioDbContext
+        )
+        {
+            _inventarioDbContext = inventarioDbContext;
+        }
+
+        public IEnumerable<Inventario> GetAll()
+        {
+            var result = new List<Inventario>();
+
+            try
+            {
+                result = _inventarioDbContext.Inventario.ToList();
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+            return result;
+        }
+
+        public Inventario Get(int id)
+        {
+            var result = new Inventario();
+
+            try
+            {
+                result = _inventarioDbContext.Inventario.Single(x => x.IdProducto == id);
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+            return result;
+        }
+
+        public bool Add(Inventario model)
+        {
+            try
+            {
+                _inventarioDbContext.Add(model);
+                _inventarioDbContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Update(Inventario model)
+        {
+            try
+            {
+                var originalModel = _inventarioDbContext.Inventario.Single(x =>
+                    x.IdProducto == model.IdProducto
+                );
+
+                originalModel.Nombre = model.Nombre;
+                originalModel.Precio = model.Precio;
+                originalModel.Descripcion = model.Descripcion;
+                originalModel.Stock = model.Stock;
+
+                _inventarioDbContext.Update(originalModel);
+                _inventarioDbContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                _inventarioDbContext.Entry(new Inventario { IdProducto = id }).State = EntityState.Deleted; ;
+                _inventarioDbContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
